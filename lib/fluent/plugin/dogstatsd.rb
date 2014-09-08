c8ac6e3d208762d7ca16449e3891a5ddd55bcbc9
Ryota Arai <ryota.arai@gmail.com>
2014-09-08 15:13:31 +0900

    bundle gem.

---
+++ lib/fluent/plugin/dogstatsd.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/dogstatsd/version"
+
+module Fluent
+  module Plugin
+    module Dogstatsd
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/dogstatsd/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Dogstatsd
+      VERSION = "0.0.1"
+    end
+  end
+end
